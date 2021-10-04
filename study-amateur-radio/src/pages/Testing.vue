29bd01636539705a1772d237f599ec760fa83f2d
Kevin E. Johnson <kevin.e.johnson@pieinsurance.com>
2021-10-04 12:29:22 -0600

    move placeholder files and turn them into vue pages

---
+++ study-amateur-radio/src/pages/Testing.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>Finding A Test Location</h1>
+    <p>Put info about finding test locations here.</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Finding A Test Location'
+  }
+}
+</script>
