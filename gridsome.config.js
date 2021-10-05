dc2b42d80ccdeda8a3c0624829f0bb57fda3aff7
Kevin E. Johnson <kevin.e.johnson@pieinsurance.com>
2021-10-05 16:13:42 -0600

    pick single spelling of "amateur"

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Study Amatueur Radio',
+  siteName: 'Study Amateur Radio',
   siteUrl: 'https://kineticsoftware.github.io/',
   pathPrefix: '/StudyAmateurRadio',
   plugins: [
