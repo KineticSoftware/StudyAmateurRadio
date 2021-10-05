b31988f25a0dc97c64f55b0c017592c8c1e922a4
Craig Young <craig.young@pieinsurance.com>
2021-10-05 15:38:21 -0600

    Enable markdown and add tailwindcss

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,30 +1,33 @@
 <template>
   <Layout>
+    <section id="container-centre" class="column centre flex-1">
+      <h1 class="page-title">Welcome to Study Amateur Radio!</h1>
+      <div class="content">
+        <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+        <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+        <h1>Hello and QSL!</h1>
 
-    <h1>Hello, world!</h1>
+        <p>
+          Would you like to get started with getting your Amateur Radio (HAM) license but just don't know where to begin?
+        </p>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-      <a href="/testing">Finding A Test Location</a>
-      <a href="/resources">Online Resources</a>
-      <a href="/plan">A Sample Study Plan</a>
-    </p>
-
+        <p class="home-links">
+          <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+          <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+          <a href="/testing">Finding A Test Location</a>
+          <a href="/resources">Online Resources</a>
+          <a href="/plan">A Sample Study Plan</a>
+        </p>
+      </div>
+    </section>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Home'
   }
 }
 </script>
