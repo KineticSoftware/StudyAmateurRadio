b53894a32ca33d242af3d2f25c68e4a821376673
Craig Young <craig.young@pieinsurance.com>
2021-10-22 15:52:34 -0600

    added resource links

--- src/pages/Resources.vue
+++ src/pages/Resources.vue
@@ -3,6 +3,11 @@
   <Layout>
     <h1>Online Resources</h1>
     <p>List online resources here.</p>
+    <ul>
+      <li><a href="https://www.arrl.org/getting-licensed" target="_blank">ARRL</a></li>
+      <li><a href="https://hamstudy.org/content/how" target="_blank">HamStudy.org</a></li>
+      <li><a href="https://www.fcc.gov/amateur-radio-service" target="_blank">FCC Information on the Amateur Radio Service</a></li>
+    </ul>
   </Layout>
 </template>
 
