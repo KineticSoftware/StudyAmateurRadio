6d9babacf0d38d0b9b3a00ce48209483deba5c29
Craig Young <craig.young@pieinsurance.com>
2021-10-22 16:01:57 -0600

    Added some study resources

--- src/pages/Plan.vue
+++ src/pages/Plan.vue
@@ -1,7 +1,18 @@
 <template>
   <Layout>
     <h1>Study Plan</h1>
-    <p>Put info about things to study and what order to study them here.</p>
+    
+      <h1>The HamStudy App</h1>
+    <p>
+      Easily the #1 choice in studying the material. The app allows you take practice exams and study until the moon turns blue. For only a couple of bucks this deal beats any book you can buy on Amazon.
+      <a href="https://hamstudy.org/appstore" target="_blank">Get the App</a>. Within the app you can study each section of an element until you're happy with your proficiency score. 
+    </p>
+
+    <h1>YouTube Playlists</h1>
+    <ul>
+      <li>Ham 2.0 <a href="https://www.youtube.com/watch?v=0wwnOPaF1qI&list=PLs_Ab58w9LI_jgoIv6SShsLACgOIly0np" target="_blank">Playlist</a></li>
+      <li>HAM Radio Crash Course <a href="https://www.youtube.com/watch?v=oc6YA9odhTs&list=PL1KAjn5rGhiwr0dtuidARiSVW8o8F9a3x" target="_blank">Playlist</a></li>
+    </ul>
   </Layout>
 </template>
 
